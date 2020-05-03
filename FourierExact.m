5d32f896691b8914416ea60c6926b315c728bbad
Aitor Amatriain <amatriai@usc.edu>
2020-05-03 01:16:14 -0700

    Update FourierExact.m

--- FourierExact.m
+++ FourierExact.m
@@ -5,4 +5,8 @@ t = linspace(0,2,25);
 [X,T] = meshgrid(x,t);
 E = -0.25*power(pi,2)*T;
 F = (80/power(pi,2))*sin(pi/2)*(exp(E)).*(sin(pi*X));
+for k = 2:5
+   F = F + (1/k^2)*(80/power(pi,2))*sin(k*pi/2)*(exp(E*(k^2))).*(sin(k*pi*X));
+end
 surf(X,T,F);
+
